1a08fa12db67f8b67687bcd7e57aca3cc447b354
Imad Ali <imadmali@gmail.com>
2016-08-23 11:13:02 -0400

    add generated quantitites

--- var.R
+++ var.R
@@ -7,14 +7,49 @@ library("vars")
 
 ### using the vars package ###
 data(Canada)
-coef(VAR(Canada[,c(2,4)], p = 1, type = "none"))
+VAR(Canada[,c(2,4)], p = 1, type = "none")
 VAR(Canada[,c(2,4)], p = 1)
 
 ### using rstan ###
 canada_data <- list()
-canada_data$Y <- Canada[,c(2,4)]
-canada_data$N <- dim(Canada[,c(2,4)])[1]
-canada_data$K <- dim(Canada[,c(2,4)])[2]
+canada_data$Y <- Canada[,c(1,2)]
+canada_data$N <- dim(Canada[,c(1,2)])[1]
+canada_data$K <- dim(Canada[,c(1,2)])[2]
+
+canada_var <- VAR(Canada[,c(1,2)], p = 1)
+predict_canada_var <- predict(canada_var)
+
 stanc("/Users/imad/Desktop/git/bayesian-econometrics/var.stan")
 stan_var <- stan(file = "/Users/imad/Desktop/git/bayesian-econometrics/var.stan", data = canada_data, cores = 4, chains = 4, iter = 2000)
-print(stan_var, digits = 3)
+traceplot(stan_var)
+
+print(stan_var, pars = c("C","A","sigma","lp__"))
+print(canada_var)
+
+stan_var_post <- list()
+stan_var_post$extract <- extract(stan_var)
+stan_var_post$predict <- cbind(
+  colMeans(stan_var_post$extract$Y_rep[,1,]),
+  colMeans(stan_var_post$extract$Y_rep[,2,])
+)
+
+# plot generated quantities
+
+par(mfrow=c(2,1))
+
+plot(c(Canada[,1]), type = "l", lwd = 2, main = "Canadian Employment (1980-2000)", xlab = "Time (Quarterly)", ylab = "Employment", xaxt = "n")
+axis(side = 1, at = seq(0, 83, 1), labels = NA)
+axis(side = 1, at = seq(0, 80, 4), labels = seq(1980,2000), tck = 0, las = 3, cex.axis = 0.8)
+lines(append(NA, stan_var_post$predict[,1]), col = "red", lwd = 2)
+
+plot(c(Canada[,2]), type = "l", lwd = 2, main = "Canadian Productivity (1980-2000)", xlab = "Time (Quarterly)", ylab = "Productivity", xaxt = "n")
+axis(side = 1, at = seq(0, 83, 1), labels = NA)
+axis(side = 1, at = seq(0, 80, 4), labels = seq(1980,2000), tck = 0, las = 3, cex.axis = 0.8)
+lines(append(NA, stan_var_post$predict[,2]), col = "cornflowerblue", lwd = 2)
+
+dev.off()
+
+var_model <- stan_model(file = "/Users/imad/Desktop/git/bayesian-econometrics/var.stan")
+opt_var <- optimizing(var_model, data = canada_data, draws = 1000, algorithm = "Newton")
+opt_var$par
+coef(VAR(Canada[,c(2,4)], p = 1, type = "none"))
--- var.stan
+++ var.stan
@@ -9,20 +9,46 @@ transformed data {
   matrix[N-1,K] Y_lead;
   matrix[N-1,K] Y_lag;
   for (n in 1:N-1) {
-    Y_lead[n] = Y[n];
+    Y_lag[n] = Y[n];
   }
   for (n in 2:N) {
-    Y_lag[n-1] = Y[n];
+    Y_lead[n-1] = Y[n];
   }
+  print("Y_lead = ", Y_lead);
+  print("Y_lag = ", Y_lag);
 }
 parameters {
-  matrix[K,K] A;   // matrix of coefficients
-  vector<lower=1>[K] sigma;
+  vector[K] C;              // constant for each equation
+  matrix[K,K] A;            // matrix of coefficients
+  vector<lower=0>[K] sigma; // standard deviation for each equation
 }
 model {
   matrix[K,N-1] eta;
-  eta = A*Y_lag';
+  for (k in 1:K) {
+    eta[k,] = C[k] + (A*Y_lag')[k,];    // linear predictor
+  }
+  #eta = C+(A*Y_lag');
   for (n in 1:N-1) {
-    Y_lead'[,n] ~ normal(eta[,n], sigma);
+    // Y_lead'[,n] ~ normal(eta[,n], sigma);              // model
+    target += normal_lpdf(Y_lead'[,n] | eta[,n], sigma);  // new syntax
+  }
+  // include priors
+  for (k in 1:K){
+    C[k] ~ normal(0,1);
+    for (j in 1:K) {
+      A[j,k] ~ normal(0,1);
+    }
+  }
+}
+generated quantities {
+  matrix[K,N-1] Y_rep;
+  matrix[K,N-1] eta_rep;
+  for (k in 1:K) {
+    eta_rep[k,] = C[k] + (A*Y_lag')[k,];
+  }
+  for (n in 1:N-1) {
+    for (k in 1:K) {
+      Y_rep[k,n] = normal_rng(eta_rep[k,n], sigma[k]);  // predictions
+    }
   }
 }
